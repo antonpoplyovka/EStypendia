c24b7c231dde4068a775c19c433b6293e04cbcee
antonpoplyovka <40170682+antonpoplyovka@users.noreply.github.com>
2020-05-05 22:11:39 +0200

    New controllers added (#2)

--- src/main/java/com/kul/Estypendia/controller/TypeOfStudentController.java
+++ src/main/java/com/kul/Estypendia/controller/TypeOfStudentController.java
@@ -1,4 +1,33 @@
 package com.kul.Estypendia.controller;
 
+import com.kul.Estypendia.model.TypeOfHousing;
+import com.kul.Estypendia.model.TypeOfStudent;
+import com.kul.Estypendia.repository.TypeOfHousingRepo;
+import com.kul.Estypendia.repository.TypeOfStudentRepo;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
 public class TypeOfStudentController {
+    private TypeOfStudentRepo typeOfStudentRepo;
+    @Autowired
+    public TypeOfStudentController(TypeOfStudentRepo typeOfStudentRepo) {
+        this.typeOfStudentRepo = typeOfStudentRepo;
+    }
+
+    @ApiOperation(value = "Get all TypesOfStudent")
+    @GetMapping("/")
+    public List<TypeOfStudent> getAllTypeOfStudent() {
+        return typeOfStudentRepo.findAll();
+    }
+
+    @ApiOperation(value = "Get TypesOfHouTypesOfStudent by id")
+    @GetMapping("/{id}")
+    public Optional<TypeOfStudent> getTypeOfStudentById(@PathVariable Integer id) {
+        return typeOfStudentRepo.findById(id);
+    }
 }
--- src/main/java/com/kul/Estypendia/repository/TypeOfStudentRepo.java
+++ src/main/java/com/kul/Estypendia/repository/TypeOfStudentRepo.java
@@ -1,9 +1,10 @@
 package com.kul.Estypendia.repository;
 
 import com.kul.Estypendia.model.TypeOfHousing;
+import com.kul.Estypendia.model.TypeOfStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TypeOfStudentRepo extends JpaRepository<TypeOfHousing,Integer> {
+public interface TypeOfStudentRepo extends JpaRepository<TypeOfStudent,Integer> {
 }
