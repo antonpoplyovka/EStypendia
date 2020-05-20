e0a8c1372b843c2de4d02fcb7c95dd95f79f5039
antonpoplyovka <antonpoplyovka@gmail.com>
2020-05-20 16:05:55 +0200

    Put endpoint for studentType

--- src/main/java/com/kul/Estypendia/controller/TypeOfStudentController.java
+++ src/main/java/com/kul/Estypendia/controller/TypeOfStudentController.java
@@ -5,6 +5,8 @@ import com.kul.Estypendia.repository.TypeOfStudentRepo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +17,7 @@ import java.util.Optional;
 @RequestMapping(value = "api/v1/typeOfStudent")
 public class TypeOfStudentController {
     private TypeOfStudentRepo typeOfStudentRepo;
+
     @Autowired
     public TypeOfStudentController(TypeOfStudentRepo typeOfStudentRepo) {
         this.typeOfStudentRepo = typeOfStudentRepo;
@@ -31,9 +34,24 @@ public class TypeOfStudentController {
     public Optional<TypeOfStudent> getTypeOfStudentById(@PathVariable Integer id) {
         return typeOfStudentRepo.findById(id);
     }
+
     @ApiOperation(value = "Add new Student Type")
     @PostMapping("/")
     public TypeOfStudent createNewStudentType(@RequestBody TypeOfStudent typeOfStudent) {
         return typeOfStudentRepo.save(typeOfStudent);
     }
+
+    @ApiOperation(value = "Edit student type Student Type")
+    @PutMapping("/{id}")
+    public ResponseEntity<TypeOfStudent> createNewStudentType(@PathVariable Integer id, @RequestBody TypeOfStudent typeOfStudent) {
+        Optional<TypeOfStudent> optionalTypeOfStudent = typeOfStudentRepo.findById(id);
+        if (!optionalTypeOfStudent.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        optionalTypeOfStudent = Optional.ofNullable(typeOfStudent);
+        typeOfStudentRepo.save(optionalTypeOfStudent.get());
+        return new ResponseEntity<>(optionalTypeOfStudent.get(), HttpStatus.OK);
+    }
+
+
 }
